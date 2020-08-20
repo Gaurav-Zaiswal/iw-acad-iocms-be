a7d816a24fd99703e08236ffffb7b67de6e65125
gaurav-zaiswal <gauravzaiswal@gmail.com>
2020-08-20 21:18:45 +0545

    modify: added namespacing

--- iocms/iocms/urls.py
+++ iocms/iocms/urls.py
@@ -3,5 +3,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls'))
+    path('users/', include('users.urls'), name="user-register")
 ]
