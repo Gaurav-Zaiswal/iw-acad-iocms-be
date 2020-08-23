4db4d084efffe55338ad15e4a457975742181b87
Dipan Bhusal <levilins40@gmail.com>
2020-08-23 17:26:09 +0545

    Added: url of media files in urlpatterns

--- iocms/iocms/urls.py
+++ iocms/iocms/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls'), name="user-register"),
     path('assignment-api/', include('assignment.urls', namespace='assignment'))
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
