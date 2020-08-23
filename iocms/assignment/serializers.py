141de2e4a773bda97ae513446bb394871d339130
Dipan Bhusal <levilins40@gmail.com>
2020-08-23 17:31:27 +0545

    Added: serializers

---
+++ iocms/assignment/serializers.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+from rest_framework.relations import HyperlinkedIdentityField 
+from users.models import Teacher, Student
+from .models import  Assignment, AssignmentByStudent
+
+
+
+
+class AssignmentCreateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Assignment
+        fields = ['id', 'teacher', 'title', 'description','points', 'deadline']
+
+   
+
+class AssignmentListSerializer(serializers.HyperlinkedModelSerializer):
+    details = HyperlinkedIdentityField(lookup_field = 'pk', view_name='assignment:details')
+    class Meta:
+        model = Assignment
+        fields = ['id', 'details']
+
+
+class AssignmentDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Assignment
+        fields = '__all__'
+
+
+class AssignmentSubmitSerializer(serializers.ModelSerializer): 
+    assignment_link = serializers.URLField()
+
+    class Meta:
+        model = AssignmentByStudent
+        fields = ['student','assignment_details','assignment_link', 'assignment_answer']
+
+
