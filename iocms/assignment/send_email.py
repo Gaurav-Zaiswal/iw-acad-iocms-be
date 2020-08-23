c5b027e18e10a7c2a890ecf5b58df0a8d7f95076
Dipan Bhusal <levilins40@gmail.com>
2020-08-23 17:29:16 +0545

    Added: Module for emailing assignment to student after it is posted

---
+++ iocms/assignment/send_email.py
@@ -0,0 +1,37 @@
+from django.core.mail import send_mail #To send email
+from django.template.loader import render_to_string
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.html import strip_tags
+
+from iocms.settings import EMAIL_HOST_USER
+
+
+
+
+def send_email(request, assignemnt_model, student_model, serializer):
+
+    id_of_new_assignemnt = serializer.data["id"]
+    new_assignment_data = assignemnt_model.objects.get(id=id_of_new_assignemnt)
+    student_email_list = []
+    for student in student_model.objects.all():
+        student_email_list.append(student.user.email)
+        
+    email_subject = f'New assignment posted by {new_assignment_data.teacher}'
+    current_site = get_current_site(request)
+
+    html_message = render_to_string('assignment/email_template/email.html',{
+        'teacher' : new_assignment_data.teacher,
+        'deadline' : new_assignment_data.deadline,
+        'domain' : current_site.domain,
+        'pk' : id_of_new_assignemnt,
+    })
+    message = strip_tags(html_message)
+    send_mail(subject=email_subject, 
+              message=message,
+              from_email=EMAIL_HOST_USER, 
+              recipient_list=student_email_list, 
+              html_message= html_message,
+              fail_silently=False
+            )
+    
+    
