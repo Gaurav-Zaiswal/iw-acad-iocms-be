0f9db037ef63988302801cb58ac231a7fcb0fe1b
Dipan Bhusal <levilins40@gmail.com>
2020-08-23 12:08:37 +0545

    Added: Admin view options for admin panel

--- iocms/users/admin.py
+++ iocms/users/admin.py
@@ -1,3 +1,20 @@
-from django.contrib import admin
 
+from django.contrib import admin
+from .models import User, Teacher, Student
 # Register your models here.
+
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+
+    list_display = ['email','is_superuser', 'is_teacher', 'is_student']
+    
+    class Meta:
+        model = User
+        
+
+
+admin.site.register([Teacher, Student])
+
+ 
