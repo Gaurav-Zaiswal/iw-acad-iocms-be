40f3320e3b4d687d5a50fecf5ff60bcff91c2918
Amar Agrawal <amaragrawal48@gmail.com>
2020-08-28 16:06:02 +0545

    Class API complete

---
+++ iocms/class/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from users.models import Teacher, Student
+from django.utils.timezone import now
+import random, string
+
+
+class Classroom(models.Model):
+    id = models.AutoField(primary_key=True)
+    class_name = models.CharField(max_length=150)
+    class_description = models.TextField(blank=True, null=True)
+    created_by = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    creation_date = models.DateTimeField(default=now)
+    is_class_code_enabled = models.BooleanField(default=True)
+    class_code = models.CharField(max_length=8)
+
+    def __str__(self):
+        return self.class_name[0:10]
+
+    def save(self, *args, **kwargs):
+        self.class_code = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
+        super(Classroom, self).save(*args, **kwargs)
+
+    class Meta:
+        ordering = ['creation_date']
+
+
+class ClassroomStudents(models.Model):
+    classroom_id = models.OneToOneField(Classroom, on_delete=models.CASCADE)
+    enrolled_student_id = models.ManyToManyField(Student, blank=True)
+
+    def __str__(self):
+        return self.classroom_id.class_name[0:10]
---
+++ iocms/class/views.py
@@ -0,0 +1,117 @@
+from django.http import Http404
+from django.core.exceptions import PermissionDenied
+
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+
+from users.permissions import IsTeacherUser
+
+from .models import Classroom, ClassroomStudents
+from .serializers import ClassroomCreateSerializer, ClassroomDetailSerializer, ClassroomListSerializer, \
+    ClassroomAddSerializer
+
+
+class ClassroomView(APIView):
+
+    def get(self, request, **kwargs):
+        query = Classroom.objects.all()
+        serializer_context = {
+            'request': request,
+        }
+        serializer = ClassroomListSerializer(query, context=serializer_context, many=True)
+
+        return Response(serializer.data)
+
+
+class ClassroomCreateView(APIView):
+    permission_classes = [IsTeacherUser]
+
+    def post(self, request):
+        serializer = ClassroomCreateSerializer(data=request.data)
+        request.data['created_by'] = request.user.id
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# Classroom detail
+class ClassroomDetailView(APIView):
+    permission_classes = [IsTeacherUser]
+
+    def get_object(self, pk):
+        try:
+            return Classroom.objects.get(pk=pk)
+        except Classroom.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        query = self.get_object(pk)
+        serializer = ClassroomDetailSerializer(query)
+        if serializer.data["created_by"] == request.user.id:
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            raise PermissionDenied("You do not have permission to view classes of other users.")
+
+
+# Classroom List
+class ClassroomListView(APIView):
+
+    def get(self, request):
+        user = request.user
+        if user.is_teacher:
+            query = Classroom.objects.filter(created_by_id=user.id)
+            serializer = ClassroomListSerializer(query, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            # query for list of class where student is enrolled
+            enrolled_student = ClassroomStudents.objects.filter(enrolled_student_id__in=[request.user.id])
+            # example O/P from above <QuerySet [<ClassroomStudents: class12>, <ClassroomStudents: class1>,
+            # <ClassroomStudents: New class >]>
+            # query to extract Classroom model fields by passing classroom_id
+            student_enrolled_class_list = [classroom.classroom_id for classroom in enrolled_student]
+            serializer = ClassroomListSerializer(student_enrolled_class_list, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class ClassroomAddView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        query = request.data["class_code"]
+        serializer = ClassroomAddSerializer(data=request.data)
+        classroom = Classroom.objects.get(class_code=query)  # Getting information of the class
+        current_user_id = request.user.id
+        if classroom.is_class_code_enabled:
+            request.data['classroom_id'] = classroom.id
+            try:
+                classroomStudent = ClassroomStudents.objects.get(classroom_id=classroom.id)
+            except ClassroomStudents.DoesNotExist:
+                classroomStudent = None
+
+            if classroomStudent is None:
+                request.data['enrolled_student_id'] = [current_user_id, ]
+                if serializer.is_valid():
+                    serializer.save()
+                    return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+            else:
+
+                list_of_enrolled_student = classroomStudent.enrolled_student_id.all()  # Find enrolled students
+                list_of_enrolled_student_id = [student.user.id for student in
+                                               list_of_enrolled_student]  # Collect students' id
+
+                if current_user_id in list_of_enrolled_student_id:
+                    return Response({"message": "student already joined", "email": request.user.email},
+                                    status=status.HTTP_201_CREATED)
+                else:
+                    classroomStudent.enrolled_student_id.add(current_user_id)
+                    return Response({"message": "Student joined to class successfully", "email": request.user.email},
+                                    status=status.HTTP_201_CREATED)
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            raise PermissionDenied("You do not have permission to view classes of other users.")
