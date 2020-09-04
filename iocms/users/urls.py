9b2d196d3271e27fc762663768b59dd9aa9c4d92
Dipan Bhusal <levilins40@gmail.com>
2020-09-04 11:03:21 +0545

    Added: Userview and url for UserView

--- iocms/users/urls.py
+++ iocms/users/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views as auth_views
 
-from .views import CreateStudentView, CreateTeacherView
+from .views import CreateStudentView, CreateTeacherView, UserView
 
 
 app_name = "users"
@@ -11,9 +11,13 @@ app_name = "users"
 router = DefaultRouter()
 router.register('api/student-register', CreateStudentView, basename='StudentModel')
 router.register('api/teacher-register', CreateTeacherView, basename='TeacherModel')
+# router.register('api/user-info', UserView, basename = 'UserModel')
 
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api/login/', auth_views.obtain_auth_token, name='login')
+    path('api/login/', auth_views.obtain_auth_token, name='login'),
+    path('api/user-info', UserView.as_view(), name = 'user-info')
+ 
 ]
+ 
