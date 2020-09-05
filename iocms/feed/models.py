9d73d6a780b4be68a7845228109419afeed6a206
gaurav-zaiswal <gauravzaiswal@gmail.com>
2020-09-05 10:47:51 +0545

    fix: model attribute

--- iocms/feed/models.py
+++ iocms/feed/models.py
@@ -1,16 +1,15 @@
 from django.db import models
 from django.utils.timezone import now
 
-
-from users.models import Teacher
+from users.models import Teacher, User
 from assignment.models import Assignment
 from classroom.models import Classroom
 
 
 class ClassroomFeed(models.Model):
     classroom_id = models.ManyToManyField(Classroom, blank=True)
-    assignment_id = models.OneToOneField(Assignment, on_delete=models.CASCADE, blank=True)
-    posted_by = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    assignment_id = models.OneToOneField(Assignment, on_delete=models.CASCADE, blank=True, null=True)
+    posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
     assignment_title = models.CharField(max_length=150)
     assignment_description = models.CharField(max_length=500)
